2f05c353a0efa655b0fddb1eb03c22575eacf60b
prabahar <prabahar.ravichandran@agr.gc.ca>
2025-03-19 16:11:24 +0000

    Script for fine-tuning detectron2

---
+++ Apptainer/detectron2/detectron2_sandbox/home/appuser/scripts/finetuning.py
@@ -0,0 +1,34 @@
+from detectron2.data.datasets import register_coco_instances
+from detectron2.config import get_cfg
+from detectron2.engine import DefaultTrainer
+import os
+
+register_coco_instances("my_dataset_train", {}, "/home/appuser/dataset/updated_train.json", "")
+register_coco_instances("my_dataset_val", {}, "/home/appuser/dataset/updated_train.json", "")
+
+cfg = get_cfg()
+cfg.merge_from_file("/home/appuser/detectron2_repo/configs/COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml")
+
+cfg.DATASETS.TRAIN = ("my_dataset_train",)
+cfg.DATASETS.TEST = ("my_dataset_val",)
+
+# Set number of classes in your dataset
+cfg.MODEL.ROI_HEADS.NUM_CLASSES = 1  # e.g., 1 if you have one class
+
+cfg.DATALOADER.NUM_WORKERS = 2
+
+# Set weights for pre-trained model (COCO or your custom model)
+cfg.MODEL.WEIGHTS = "/home/appuser/models/model_final_f10217.pkl"  # Start from ImageNet weights or COCO weights
+
+cfg.SOLVER.IMS_PER_BATCH = 2
+cfg.SOLVER.BASE_LR = 0.00025  # Learning rate, tune as needed
+cfg.SOLVER.MAX_ITER = 3000    # Adjust iterations as per dataset size
+cfg.SOLVER.STEPS = []         # Disable learning rate decay
+
+cfg.OUTPUT_DIR = "/mnt/PhenomicsProjects/Detectron2/Apptainer/detectron2/detectron2_sandbox/home/appuser/models"
+os.makedirs(cfg.OUTPUT_DIR, exist_ok=True)
+
+# Train the model
+trainer = DefaultTrainer(cfg)
+trainer.resume_or_load(resume=False)
+trainer.train()
